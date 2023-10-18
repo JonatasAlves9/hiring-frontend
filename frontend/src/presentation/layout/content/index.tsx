97223f56d52761d5dcfb4906a2cb5e437e569bac
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-10-18 17:06:05 -0300

    feat: create ContentWrapper

---
+++ frontend/src/presentation/layout/content/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Wrapper} from "./styles.ts";
+
+interface IProps {
+    children: React.JSX.Element
+}
+
+export const Content = ({children}: IProps) => {
+    return (
+        <Wrapper>
+            {children}
+        </Wrapper>
+    )
+}
--- frontend/src/presentation/layout/pageWrapper/index.tsx
+++ frontend/src/presentation/layout/pageWrapper/index.tsx
@@ -1,12 +1,15 @@
 import {Wrapper} from "./styles.ts";
 import {Outlet} from "react-router-dom";
 import {TopBar} from "../topbar";
+import {Content} from "../content";
 
 export const PageWrapper = () => {
     return (
         <Wrapper>
             <TopBar/>
-            <Outlet/>
+            <Content>
+                <Outlet/>
+            </Content>
         </Wrapper>
     )
 }
