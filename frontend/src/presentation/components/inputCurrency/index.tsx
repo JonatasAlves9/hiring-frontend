85ced2926ab97b166ae562174552fccbcdea9e48
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-10-18 22:14:03 -0300

    feat: put mask in input

--- frontend/src/presentation/components/inputCurrency/index.tsx
+++ frontend/src/presentation/components/inputCurrency/index.tsx
@@ -1,9 +1,21 @@
 import {Input, Label, Wrapper} from "./styles.ts";
+import {useState} from "react";
 
 export const InputCurrency = () => {
+
+    const [value, setValue] = useState('');
+
+    const handleChange = (e: any) => {
+        let val = e.target.value;
+        val = val.replace(/\D/g, "");
+        val = new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(val / 100);
+        setValue(val);
+    };
+
+
     return (
         <Wrapper>
-            <Input placeholder={"R$ 1.500,00"} type={'number'}/>
+            <Input value={value} onChange={handleChange} placeholder="R$ 1.500,00"/>
             <Label>BRL</Label>
         </Wrapper>
     )
