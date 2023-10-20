72fdfc4b58e8f735f3dcf8d9acada0077fdb2e20
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-10-20 09:07:09 -0300

    fix: resolve error when go back page and search new stock mode view in dashboard is wrong

--- frontend/src/presentation/hooks/useStocks.tsx
+++ frontend/src/presentation/hooks/useStocks.tsx
@@ -170,6 +170,7 @@ const StockProvider = ({ children, stock }: IProps) => {
     setGainsOfStock(undefined);
     setStockedCompared(undefined);
     setGainsOfStock(undefined);
+    setStockToCompare([]);
     setLoadingStockDetail(STATUS_REQUEST.LOADING);
   }, [setStockDetail, setGainsOfStock, setStockedCompared, setGainsOfStock]);
 
