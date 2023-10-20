aeec17cd7e649456c3c2dce65fde5e446803f9f2
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-10-19 23:13:42 -0300

    feat: config and create test above button

---
+++ frontend/cypress/component/button.cy.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { mount } from 'cypress/react18';
+import { Button } from '../../src/presentation/components/button';
+import { themeDark } from '../../src/presentation/style/themes';
+import 'react-activity/dist/library.css';
+
+it('renders correctly', () => {
+  mount(
+    <ThemeProvider theme={themeDark}>
+      <Button />
+    </ThemeProvider>,
+  );
+
+  cy.get('div').should('exist');
+});
+
+it('should display the spinner when loading', () => {
+  mount(
+    <ThemeProvider theme={themeDark}>
+      <Button loading={true} />
+    </ThemeProvider>,
+  );
+
+  cy.get('div[data-testid="rai-activity-indicator"]').should('exist');
+});
